328533f92274e898c09e6a5ce1e24cd5f0d16f9f
tzboril <tzboril@cisco.com>
2020-01-25 14:42:21 +0000

    Fixing paths to files.

--- scarfshop/static/corporate/scripts/layout.js
+++ scarfshop/static/corporate/scripts/layout.js
@@ -272,9 +272,9 @@ var Layout = function () {
     
         // handle theme colors
         var setColor = function (color) {
-            $('#style-color').attr("href", "../../assets/corporate/css/themes/" + color + ".css");
-            $('.corporate .site-logo img').attr("src", "../../assets/corporate/img/logos/logo-corp-" + color + ".png");
-            $('.ecommerce .site-logo img').attr("src", "../../assets/corporate/img/logos/logo-shop-" + color + ".png");
+            $('#style-color').attr("href", "static/corporate/css/themes/" + color + ".css");
+            $('.corporate .site-logo img').attr("src", "static/corporate/img/logos/logo-corp-" + color + ".png");
+            $('.ecommerce .site-logo img').attr("src", "static/corporate/img/logos/logo-shop-" + color + ".png");
         }
 
         $('.icon-color', panel).click(function () {
