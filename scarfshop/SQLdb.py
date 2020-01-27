39a70f72c94be84a50c47c65cdc452d56443c12c
bustedstr <55255448+bustedstr@users.noreply.github.com>
2020-01-27 12:43:41 +0000

    sqldb update v3

--- scarfshop/SQLdb.py
+++ scarfshop/SQLdb.py
@@ -7,7 +7,11 @@ try:
                                          user='root',
                                          password='Av136.356hP0x')
 
-    sql_select_Query = "select * from tblAddress"
+    sql_select_Query = "SELECT `tblProduct`.`productID`, `tblProduct`.`productName`, `tblProduct`.`quantity`, `tblCustomer`.`email`, `tblTransactions`.`totalPrice`
+FROM `tblCustomer`
+    LEFT JOIN `tblTransactions` ON `tblTransactions`.`email` = `tblCustomer`.`email`
+    LEFT JOIN `tblProduct` ON `tblTransactions`.`productID` = `tblProduct`.`productID`"
+
     cursor = connection.cursor()
     cursor.execute(sql_select_Query)
     records = cursor.fetchall()
@@ -15,13 +19,11 @@ try:
 
     print("\nPrinting each Address record")
     for row in records:
-        print("Streetname= ", row[0],)
-        print("Streetno = ", row[1])
-        print("postcode  = ", row[2])
-        print("city  = ", row[3])
-        print("country  = ", row[4])
-        print("email = ", row[5])
-        print("phoneNo  = ", row[6], "\n")
+        print("pID= ", row[0],)
+        print("pName = ", row[1])
+        print("quan  = ", row[2])
+        print("email  = ", row[3])
+        print("tPrice  = ", row[4], "\n")
 
 
 except Error as e:
@@ -31,3 +33,14 @@ finally:
         cursor.close()
         connection.close()
         print("MySQL connection is closed")
+
+#query for returning all records in address tbl with all fields
+# print("\nPrinting each Address record")
+#     for row in records:
+#         print("Streetname= ", row[0],)
+#         print("Streetno = ", row[1])
+#         print("postcode  = ", row[2])
+#         print("city  = ", row[3])
+#         print("country  = ", row[4])
+#         print("email = ", row[5])
+#         print("phoneNo  = ", row[6], "\n")
