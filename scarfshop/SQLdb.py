34c57210c47e89ca95d2fa1f28e1d93416ff8660
bustedstr <55255448+bustedstr@users.noreply.github.com>
2020-01-27 12:12:34 +0000

    sqldb update v2

--- scarfshop/SQLdb.py
+++ scarfshop/SQLdb.py
@@ -6,13 +6,23 @@ try:
                                          database='webApp_DB',
                                          user='root',
                                          password='Av136.356hP0x')
-    if connection.is_connected():
-        db_Info = connection.get_server_info()
-        print("Connected to MySQL Server version ", db_Info)
-        cursor = connection.cursor()
-        cursor.execute("select database();")
-        record = cursor.fetchone()
-        print("You're connected to database: ", record)
+
+    sql_select_Query = "select * from tblAddress"
+    cursor = connection.cursor()
+    cursor.execute(sql_select_Query)
+    records = cursor.fetchall()
+    print("Total number Addresses: ", cursor.rowcount)
+
+    print("\nPrinting each Address record")
+    for row in records:
+        print("Streetname= ", row[0],)
+        print("Streetno = ", row[1])
+        print("postcode  = ", row[2])
+        print("city  = ", row[3])
+        print("country  = ", row[4])
+        print("email = ", row[5])
+        print("phoneNo  = ", row[6], "\n")
+
 
 except Error as e:
     print("Error while connecting to MySQL", e)
@@ -21,4 +31,3 @@ finally:
         cursor.close()
         connection.close()
         print("MySQL connection is closed")
-
